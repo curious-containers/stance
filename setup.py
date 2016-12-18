f9bb9a793b166b7c1f101e0c9d545549612dccba
Christoph Jansen <christoph@gnork.org>
2016-12-19 00:29:02 +0100

    updated module structure and distribution info

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ from distutils.core import setup
 setup(
     name='stance',
     version='1.0',
-    summary='stance provides self-instantiating worker processes for Python 3',
-    description='stance provides self-instantiating worker processes for Python 3. It is best used with WSGI applications, like a flask web server, where the forking of processes takes place, before the user gains control.',
+    description='stance provides self-instantiating worker processes for Python 3',
     author='Christoph Jansen',
     author_email='christoph@gnork.org',
     url='https://github.com/curious-containers/stance',
     packages=['stance'],
-    license='MIT',
+    license='Apache License 2.0',
     platforms=['any'],
     install_requires=[]
 )
