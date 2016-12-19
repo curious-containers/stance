e517d41a12a2948a6955742f436747d118f192c6
Blake VandeMerwe <blake.vandemerwe@vivint.com>
2016-12-19 14:10:42 -0700

    Add documentation and structural code formatting.

--- stance/__init__.py
+++ stance/__init__.py
@@ -4,6 +4,8 @@ import time
 import atexit
 import multiprocessing.managers
 
+__all__ = ['Stance', 'StanceException']
+
 
 class _SuppressStdErr(object):
     def __init__(self, devnull):
@@ -19,51 +21,132 @@ class _SuppressStdErr(object):
         sys.stderr = self._stderr
 
 
+class StanceException(Exception):
+    """ Base Exception class for errors that occur in this module. """
+
+
 class Stance:
-    def __init__(self, clazz, port, secret='secret'):
-        self._clazz = clazz
+    """ Self-instantiating worker process. Will intelligently run a single instance
+        of a registered class, by either creating that instance or connecting to it.
+        Utilizes the :mod:`multiprocessing.managers` interface for secure local-machine
+        processes and management.
+
+        Args:
+            _class (class): Class that gets instantiated one time, and can be accessed
+                across other process instances.
+            port (int): Port that determines where an instance is created, used as a lock
+                for preventing more than one to be running at a given time.
+            secret (str): Connection secret used for TLS communication. If ``secret``
+                is left as ``None`` the default (``secret``) is used.
+    """
+    def __init__(self, _class, *, port, secret=None):
+        if secret is None:
+            secret = 'secret'
+
+        self._class = _class
         self._port = port
         self._secret = secret.encode('utf-8')
         self._instance = None
         self._new_instance = False
 
+    @classmethod
+    def create(cls, _class, *, port, secret=None, args=None, kwargs=None):
+        """ Performs a :obj:`.Stance` init and register for a given _class.
+
+            Args:
+                _class (class)
+                port (int)
+                secret (str)
+                args (tuple): arguments passed into ``_class.__init__``.
+                kwargs (dict): keyword arguments passed into ``_class.__init__``.
+
+            Returns:
+                :obj:`.Stance`
+        """
+        args = tuple(args) if args else tuple()
+        kwargs = dict(kwargs) if kwargs else dict()
+
+        o = cls(_class, port=port, secret=secret)
+        o.register(*args, **kwargs)
+        return o
+
+    @property
+    def inst(self):
+        """ Returns the instance of the underlying :obj:`._class` object. """
+        return self._instance
+
     def created_new_instance(self):
+        """ Checks if the instance was created or connected to.
+
+            Raises:
+                StanceException: when :func:`.register` hasn't been called yet.
+
+            Returns:
+                bool
+        """
         if not self._instance:
-            raise Exception('instance has not yet been registered')
+            raise StanceException('instance has not yet been registered')
         return self._new_instance
 
     def register(self, *args, **kwargs):
+        """ Creates or connects to an instance of :attr:`._class` depending
+            on whether it already exists or not. Arguments are passed to the
+            underlying ``__init__`` function when the instance is created.
+
+            Args:
+                args (tuple)
+                kwargs (dict)
+
+            Raises:
+                StanceException: when :obj:`.Stance` is unable to create or
+                    connect to an existing instance of :attr:`._class`.
+
+            Returns:
+                Instance of ``._class``.
+        """
+        class ClassManager(multiprocessing.managers.BaseManager):
+            pass
+
         if self._instance:
             return self._instance
 
-        class ClazzManager(multiprocessing.managers.BaseManager):
-            pass
-
         try:
+            # first try to connect to an existing instance
             with open(os.devnull, 'w') as devnull:
                 with _SuppressStdErr(devnull=devnull):
-                    self._connect(ClazzManager)
-        except:
+                    self._connect(ClassManager)
+
+        except (ConnectionAbortedError, ConnectionRefusedError):
+            # second, if you can't connect to an existing instance
+            # create a new networked-instance via ClassManager
             try:
                 with open(os.devnull, 'w') as devnull:
                     with _SuppressStdErr(devnull=devnull):
-                        self._start(ClazzManager, args, kwargs)
-            except:
+                        self._start(ClassManager, args, kwargs)
+
+            except Exception:
+                # lastly, re-attempt to connect to networked-instance
                 time.sleep(1)
-                self._connect(ClazzManager)
+                self._connect(ClassManager)
+
+        except Exception as e:
+            # unexpected failure, raise it
+            raise StanceException(e)
 
         return self._instance
 
-    def _connect(self, clazz_manager):
-        clazz_manager.register('get_instance')
-        manager = clazz_manager(address=('', self._port), authkey=self._secret)
+    def _connect(self, class_manager):
+        """ Private method to connect to external process. """
+        class_manager.register('get_instance')
+        manager = class_manager(address=('', self._port), authkey=self._secret)
         manager.connect()
         self._instance = manager.get_instance()
 
-    def _start(self, clazz_manager, args, kwargs):
-        instance = self._clazz(*args, **kwargs)
-        clazz_manager.register('get_instance', callable=lambda: instance)
-        manager = clazz_manager(address=('', self._port), authkey=self._secret)
+    def _start(self, class_manager, args, kwargs):
+        """ Private method to start an external process. """
+        instance = self._class(*args, **kwargs)
+        class_manager.register('get_instance', callable=lambda: instance)
+        manager = class_manager(address=('', self._port), authkey=self._secret)
         manager.start()
         atexit.register(manager.shutdown)
         self._instance = manager.get_instance()
